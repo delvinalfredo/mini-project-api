25ac516385a80f03c2300bd9e990014897806ddc
delvinalfredo <delvinalfredo@gmail.com>
2022-04-09 19:01:45 +0700

    remove env

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -15,12 +15,13 @@ exports.getProductId = (req,res) =>{
     // console.log('get product id');
     modelProduct.getListProductId(req.params.id, (err, merchant_service) =>{
         if(err){
-            res.status(400).send(err);
+            res.status(401).send(err);
         } else{
             res.status(200).send(merchant_service);
         }  
     })
 }
+
 
 
 exports.getProductId
